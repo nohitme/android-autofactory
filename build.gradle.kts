85043c9446d0e9c5f16f50a67b4f38ad2958da75
Eric Lin <mlin@groupon.com>
2018-12-24 17:39:17 -0800

    update android gradle plugin to 3.4.0-alpha09

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
         google()
@@ -8,10 +7,8 @@ buildscript {
     val kotlinVersion = "1.3.11"
 
     dependencies {
-        classpath("com.android.tools.build:gradle:3.4.0-alpha07")
+        classpath("com.android.tools.build:gradle:3.4.0-alpha09")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
     }
 }
 
